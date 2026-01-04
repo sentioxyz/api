c9671570e9a53d5e7ae512b9b5600ca5a73b8f15
Poytr1 <pooytr1@gmail.com>
2026-01-04 23:49:33 +0800

    fix the test

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -2,13 +2,13 @@ import { client, WebService } from "../src/index.js";
 import test from "node:test";
 import assert from "assert";
 
-test("getProjectList", async () => {
+test("listDashboards", async () => {
   const apiKey = process.env.SENTIO_API_KEY;
   assert(apiKey, "API key can't be found for the test");
 
   client.setConfig({
     auth: process.env.SENTIO_API_KEY,
   });
-  const projects = await WebService.getProjectList();
-  console.log(projects);
+  const dashboards = await WebService.listDashboards();
+  console.log(dashboards);
 });
